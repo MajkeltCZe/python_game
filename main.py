7f902cc11ab7480a3f5037a2aa57922cc2970af0
MajkeltCZe <majkeltcz@gmail.com>
2023-01-10 17:54:15 +0100

    zacatek

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-import pygame as p
+import pygame as pg
+import random
 from sys import exit
+import pymunk
+from settings import *
+import pymunk.pygame_util
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.clock = pg.time.Clock()
+        self.screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+        pg.display.set_caption('Narrative Disaster')
+        self.running = True
+
+    def new(self):
+        pass
+    def events(self):
+        self.events_list = pg.event.get()
+        for event in self.events_list:
+            if event.type == pg.QUIT:
+                self.running = False
 
 
-p.init()
-screen = p.display.set_mode((800,400))
-p.display.set_caption('Narrative Disaster')
-clock = p.time.Clock()
+    def update(self):
+        self.screen.fill("red")
+        self.clock.tick(FPS)
 
-while True:
-    for event in p.event.get():
-        if event.type == p.QUIT:
-            p.quit()
-            exit()
-    p.display.update()
-    clock.tick(60)
+
+    def run(self):
+        while self.running:
+                self.events()
+                self.update()
+
+        # close pygame
+        pg.quit()
+        exit()
+
+
+
+
+def main():
+    g = Game()
+    g.run()
+
+
+if __name__ == '__main__': main()
